06a51cccb8d2d6fb3e7c2c80990f08e0e429477f
Marília Cardoso <50154330+MariliaCardoso@users.noreply.github.com>
2024-04-28 21:21:57 -0300

    Create Deasafio.js
    **O Que deve ser utilizado**
    
    - Variáveis
    - Operadores
    - Laços de repetição
    - Estruturas de decisões
    
    ## Objetivo
    
    Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
    
    Se XP for menor do que 1.000 = Ferro
    Se XP for entre 1.001 e 2.000 = Bronze
    Se XP for entre 2.001 e 5.000 = Prata
    Se XP for entre 5.001 e 7.000 = Ouro
    Se XP for entre 7.001 e 8.000 = Platina
    Se XP for entre 8.001 e 9.000 = Ascendente
    Se XP for entre 9.001 e 10.000= Imortal
    Se XP for maior ou igual a 10.001 = Radiante
    
    ## Saída
    
    Ao final deve se exibir uma mensagem:
    "O Herói de nome **{nome}** está no nível de **{nivel}**"

---
+++ Deasafio.js
@@ -0,0 +1,32 @@
+console.log("Digite o nome do seu herói")
+let nickname = "Pedrinho "
+
+let level = ["Ferro " , "Bronze " , "Prata " , "Ouro" , "Platina " , "Ascendente" , "Imortal" , "Radiante"]
+const XP = 2742
+const limiteInferior = 0;
+const limiteSuperior = 1001;
+
+if ( XP < 1001) 
+    console.log("O Herói de nome " + nickname + "está no nível de Ferro");
+
+else if ( XP < 2001) 
+    console.log("O Herói de nome " + nickname + "está no nível de Bronze");
+    
+else if ( XP < 3001) 
+    console.log("O Herói de nome " + nickname + "está no nível de Prata");
+
+else if ( XP < 4001) 
+    console.log("O Herói de nome " + nickname + "está no nível de Ouro");
+
+else if ( XP < 5001) 
+    console.log("O Herói de nome " + nickname + "está no nível de Platina");
+
+    else if ( XP < 6001) 
+    console.log("O Herói de nome " + nickname + "está no nível de Ascendente");
+
+   else if ( XP < 7001) 
+    console.log("O Herói de nome " + nickname + "está no nível de Imortal");
+
+else if ( XP < 7001) 
+    console.log("O Herói de nome " + nickname + "está no nível de Radiante");
+    
