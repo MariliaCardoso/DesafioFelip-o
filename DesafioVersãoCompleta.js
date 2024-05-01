af6f831a50dcf910ebf326132d029c60023c9d5d
Marília Cardoso <50154330+MariliaCardoso@users.noreply.github.com>
2024-05-01 15:11:50 -0300

    Update DesafioVersãoCompleta.js

--- DesafioVersãoCompleta.js
+++ DesafioVersãoCompleta.js
@@ -1,31 +1,40 @@
-console.log("Digite o nome do seu herói");
+//Desafio Classificador de nível de Herói
 
-let nickname = "Felipe";
+console.log("Digite o nome do seu herói")
 
-console.log("Agora vamos descobrir o nível do seu herói!");
+let nickname = "Felipe "
 
-// Array de experiências para verificar
-let experiencias = [500, 1500, 2500, 3500, 4500, 5500, 6500, 7500];
+console.log("Agora vamos descobrir o nível do seu herói!")
 
-for (let i = 0; i < experiencias.length; i++) {
-    let XP = experiencias[i];
-    if (XP < 1001) {
-        console.log("O Herói de nome " + nickname + " está no nível de Ferro");
-    } else if (XP < 2001) {
-        console.log("O Herói de nome " + nickname + " está no nível de Bronze");
-    } else if (XP < 3001) {
-        console.log("O Herói de nome " + nickname + " está no nível de Prata");
-    } else if (XP < 4001) {
-        console.log("O Herói de nome " + nickname + " está no nível de Ouro");
-    } else if (XP < 5001) {
-        console.log("O Herói de nome " + nickname + " está no nível de Platina");
-    } else if (XP < 6001) {
-        console.log("O Herói de nome " + nickname + " está no nível de Ascendente");
-    } else if (XP < 7001) {
-        console.log("O Herói de nome " + nickname + " está no nível de Imortal");
-    } else {
-        console.log("O Herói de nome " + nickname + " está no nível de Radiante");
-    }
-}
 
-console.log("Obrigado por checar o nível do seu herói!");
+const XP = 6000
+
+if ( XP < 1001) 
+    console.log("O Herói de nome " + nickname + "está no nível de Ferro");
+
+
+  
+    else if ( XP >= 2001) 
+    console.log("O Herói de nome " + nickname + "está no nível de Bronze");
+    
+else if ( XP >= 3001) 
+    console.log("O Herói de nome " + nickname + "está no nível de Prata");
+    
+
+else if ( XP >= 4001) 
+    console.log("O Herói de nome " + nickname + "está no nível de Ouro");
+
+else if ( XP >= 5001) 
+    console.log("O Herói de nome " + nickname + "está no nível de Platina");
+
+    else if ( XP >= 6001) 
+    console.log("O Herói de nome " + nickname + "está no nível de Ascendente");
+
+   else if ( XP >= 7001) 
+    console.log("O Herói de nome " + nickname + "está no nível de Imortal");
+
+else ( XP >= 8001) 
+    console.log("O Herói de nome " + nickname + "está no nível de Radiante");
+    
+
+console.log("Obrigado por checar o nível do seu herói!")
